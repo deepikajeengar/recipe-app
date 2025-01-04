f9373e9ce7ef81abcd67e8a6d271dfb36b763f34
bhaveshgoswami02 <bhaveshgoswami2381998@gmail.com>
2025-01-04 20:10:37 +0530

    pwa intigration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,6 +33,7 @@ import { AddCategoryComponent } from './categories-form/add-category/add-categor
 import { AngularEditorModule } from '@kolkov/angular-editor';
 import { HttpClientModule} from '@angular/common/http';
 import { Cloudinary, CloudinaryModule } from '@cloudinary/angular-5.x';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -79,7 +80,13 @@ import { Cloudinary, CloudinaryModule } from '@cloudinary/angular-5.x';
       measurementId: "G-TWFPTN65NY",
      })),
     provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
